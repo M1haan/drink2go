bc3a5a8f91454a7d9396007ba6d97d6075a6bf78
Михаил <75782356+M1haan@users.noreply.github.com>
2023-06-22 23:39:27 +0700

    сетки

--- source/js/script.js
+++ source/js/script.js
@@ -1,1 +1,34 @@
-/* в этот файл добавляет скрипты*/
+const mainNav = document.querySelector('.nav-list');
+const navToggle = document.querySelector('.header__toggle');
+
+mainNav.classList.remove('nav-list--nojs');
+navToggle.classList.remove('header__toggle--nojs');
+
+if (!mainNav.classList.contains('site-list--closed')) {
+  mainNav.classList.add('site-list--closed');
+}
+
+navToggle.addEventListener('click', () => {
+  mainNav.classList.toggle('nav-list--closed');
+  navToggle.classList.toggle('header__toggle--open');
+  navToggle.classList.toggle('header__toggle--closed');
+});
+
+const mainSlider = document.querySelector('.about-slider__wrapper');
+mainSlider.classList.remove('about-slider__wrapper--nojs');
+
+const swiper = new Swiper('.about-slider__container', {
+  navigation: {
+    nextEl: '.about-slider__button-next',
+    prevEl: '.about-slider__button-prev',
+  },
+
+  pagination: {
+    el: '.swiper-pagination',
+    clickable: true,
+
+    renderBullet: function (index, className) {
+      return `<span class="${className}"></span>`;
+    },
+  }
+});
